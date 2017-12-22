70d5fb1456cdc51d7fa5f37425750c868791498e
Scot Matson <sjpmatson@gmail.com>
2017-12-21 19:18:00 -0800

    Added test cases for Vector class, and expanded Vector class to include more comprehensive value checking

--- pasha/vector.py
+++ pasha/vector.py
@@ -4,12 +4,14 @@ class Vector(object):
 
     def __init__(self, coordinates):
         try:
-            if not coordinates:
-                raise ValueError
+            if not all([
+                coordinates,
+                all(isinstance(element, int) for element in coordinates)
+            ]): raise ValueError
             self.coordinates = tuple(coordinates)
             self.dimension = len(coordinates)
         except ValueError :
-            raise ValueError('The coordinates must be nonempty')
+            raise ValueError('The coordinates must be a nonempty list of integers.')
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,1 +1,13 @@
-import pasha
+import unittest
+from pasha.vector import Vector
+
+class TestVectorClass(unittest.TestCase):
+
+    def test_vector(self):
+        self.assertTrue(isinstance(Vector([1, 2, 3]), Vector))
+        with self.assertRaises(TypeError):
+            Vector(1, 2, 3)
+        with self.assertRaises(ValueError):
+            Vector([])
+        with self.assertRaises(ValueError):
+            Vector(['1', 2, 3])
