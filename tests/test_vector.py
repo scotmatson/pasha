37ceaf96b678837f6139e0a1bfd1e010b04744f0
Scot Matson <sjpmatson@gmail.com>
2017-12-23 00:01:31 -0800

    Broke apart tests

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -3,14 +3,20 @@ from pasha.vector import Vector
 
 class TestVectorClass(unittest.TestCase):
 
-    def test_vector(self):
+    def test_isvector(self):
         self.assertTrue(isinstance(Vector([1, 2, 3]), Vector))
-        with self.assertRaises(TypeError):
-            Vector(1, 2, 3)
-        with self.assertRaises(ValueError):
-            Vector([])
+
+    def test_non_integer(self):
         with self.assertRaises(ValueError):
             Vector(['1', 2, 3])
+
+    def test_empty_vector(self):
+        with self.assertRaises(ValueError):
+            Vector([])
+
+    def test_noniterable(self):
+        with self.assertRaises(TypeError):
+            Vector(1, 2, 3)
 
     def test_str(self):
         self.assertTrue('Vector: (1, 2, 3)' == Vector([1, 2, 3]).__str__())
